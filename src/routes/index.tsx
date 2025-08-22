a697469271489074e4e0bd0a79f8fb2f3329afab
thyeone <thyeonee@gmail.com>
2025-08-22 22:32:58 +0900

    chore: 패키지 이전

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,13 +2,29 @@ import { Calendar } from '@/headless/Calendar';
 import { Box } from '@/headless/ui/Box';
 import { Spacing } from '@/headless/ui/Spacing';
 import 'dayjs/locale/ko';
-import { EmblaCarousel } from 'embla-kit';
+import { EmblaCarousel } from '@thyeone/embla';
 
-export default function Main() {
+export default function Index() {
   return (
-    <Box className="w-full  h-dvh px-20 py-16">
-      <Spacing size={48} />
-      <Calendar />
-    </Box>
+    <EmblaCarousel.Root
+      options={{
+        stopPropagation: true,
+      }}
+      className="p-4"
+    >
+      <EmblaCarousel.Content>
+        <EmblaCarousel.Root
+          options={{
+            stopPropagation: true,
+          }}
+        >
+          <EmblaCarousel.Content>
+            <EmblaCarousel.Item className="size-[300px] border border-gray-100 bg-gray-50 rounded-md">
+              dd
+            </EmblaCarousel.Item>
+          </EmblaCarousel.Content>
+        </EmblaCarousel.Root>
+      </EmblaCarousel.Content>
+    </EmblaCarousel.Root>
   );
 }
