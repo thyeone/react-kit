239b838d5510d21943edb0fecc447c91cc0efa5d
thyeone <thyeonee@gmail.com>
2025-08-30 23:11:56 +0900

    biome 설정

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,5 @@
-import { Calendar } from '@/headless/Calendar';
-import { Box } from '@/headless/ui/Box';
-import { Spacing } from '@/headless/ui/Spacing';
-import 'dayjs/locale/ko';
-import { EmblaCarousel } from '@thyeone/embla';
+import 'dayjs/locale/ko'
+import { EmblaCarousel } from '@thyeone/embla'
 
 export default function Index() {
   return (
@@ -19,12 +16,12 @@ export default function Index() {
           }}
         >
           <EmblaCarousel.Content>
-            <EmblaCarousel.Item className="size-[300px] border border-gray-100 bg-gray-50 rounded-md">
+            <EmblaCarousel.Item className="size-[300px] rounded-md border border-gray-100 bg-gray-50">
               dd
             </EmblaCarousel.Item>
           </EmblaCarousel.Content>
         </EmblaCarousel.Root>
       </EmblaCarousel.Content>
     </EmblaCarousel.Root>
-  );
+  )
 }
